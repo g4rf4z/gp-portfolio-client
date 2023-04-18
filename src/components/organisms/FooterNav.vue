8a4587d283c5ec533868e316a1e4daf22f1de5ea
g4rf4z <gregoire.pinte@gmail.com>
2023-04-19 00:55:20 +0200

    19/04/23 - Add GitHub repository statistics
    - Create a store to make a GET request to GitHub API.
    - Retrieve 'forks' and 'stars' fields.
    - Import Font Awesome icons.
    - Update 'postcss' and 'vite' packages.

--- src/components/organisms/FooterNav.vue
+++ src/components/organisms/FooterNav.vue
@@ -1,35 +1,80 @@
 <template>
   <div class="footer-wrapper">
-    <div class="logo-wrapper">
-      <icon-link-molecule
-        to="/"
-        weight="fas"
-        icon="cube"
-        class="logo"
-      ></icon-link-molecule>
-    </div>
     <link-atom is="a" to="https://github.com/g4rf4z/">
-      <small>© 2022 - 2023 - Grégoire Pinte</small>
+      <small class="copyright">© 2022 - 2023 - Grégoire Pinte</small>
     </link-atom>
+    <div class="github-statistics">
+      <small class="star">
+        <icon-atom
+          weight="fas"
+          icon="star"
+          size="xs"
+          class="star-icon"
+        ></icon-atom
+        >{{ stars }}
+      </small>
+      <small class="fork">
+        <icon-atom
+          weight="fas"
+          icon="code-fork"
+          size="xs"
+          class="fork-icon"
+        ></icon-atom
+        >{{ forks }}
+      </small>
+    </div>
   </div>
 </template>
 
 <script setup>
-import IconLinkMolecule from "#/molecules/IconLink.vue";
 import LinkAtom from "#/atoms/Link.vue";
+import IconAtom from "#/atoms/Icon.vue";
+
+import { computed } from "vue";
+import { useGithubStore } from "@/store/githubStore";
+
+const githubStore = useGithubStore();
+
+const stars = computed(() => githubStore.stars);
+const forks = computed(() => githubStore.forks);
+
+const retrieveRepository = async () => {
+  try {
+    await githubStore.retrieveRepository();
+  } catch (error) {
+    console.error(error);
+  }
+};
+retrieveRepository();
 </script>
 
 <style lang="scss" scoped>
 .footer-wrapper {
-  @apply h-20 w-full flex flex-col items-center justify-evenly;
+  @apply h-20 w-full flex flex-col items-center justify-center;
 
-  .logo {
-    @apply text-teal-200;
-  }
-
-  small {
+  .copyright {
     @apply font-mono text-indigo-200 duration-300;
     @apply hover:text-teal-200;
+  }
+
+  .github-statistics {
+    @apply flex;
+
+    .star {
+      @apply mr-1 font-mono text-indigo-200;
+
+      .star-icon {
+        @apply mr-1;
+      }
+    }
+
+    .fork {
+      @apply ml-1 font-mono text-indigo-200;
+
+      .fork-icon {
+        @apply mr-1;
+      }
+    }
   }
 }
 </style>
