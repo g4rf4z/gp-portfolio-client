cf86b3df6d1e41ba41dba2498caa0a96c7c9a6fc
g4rf4z <gregoire.pinte@gmail.com>
2023-05-17 15:12:36 +0200

    23/05 - Create 3D portrait

---
+++ src/components/atoms/Portrait.vue
@@ -0,0 +1,91 @@
+<template>
+  <div
+    ref="card"
+    class="card"
+    @mousemove="handleMousemove"
+    @mouseleave="handleMouseleave"
+  >
+    <picture ref="picture">
+      <img src="../../assets/images/portrait.png" alt="Portrait" />
+    </picture>
+    <div ref="glow" class="glow"></div>
+    <div class="frame" @mousemove.stop></div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const card = ref(null);
+const picture = ref(null);
+const glow = ref(null);
+
+let lastCall = 0;
+const delay = 25;
+
+const handleMousemove = (event) => {
+  const now = Date.now(); // Get the current time.
+
+  // If the time elapsed since the lastCall is less than the delay, handleMousemove() returns.
+  if (now - lastCall < delay) {
+    return;
+  }
+
+  lastCall = now; // Update the value of lastCall with the value of now.
+
+  let clientRect = card.value.getBoundingClientRect();
+
+  let x = event.clientX - clientRect.x;
+  let y = event.clientY - clientRect.y;
+
+  // Calculate the middle of the element.
+  let midCardWidth = clientRect.width / 2;
+  let midCardHeight = clientRect.height / 2;
+
+  // Calculate rotation angles for 3D transformation.
+  let angleY = -(x - midCardWidth) / 8;
+  let angleX = (y - midCardHeight) / 8;
+
+  // Calculate coordinates for the radial gradient of the glow effect.
+  let glowX = (x / clientRect.width) * 100;
+  let glowY = (y / clientRect.height) * 100;
+
+  picture.value.style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg) scale(1.1)`;
+  glow.value.style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg) scale(1.1)`;
+  glow.value.style.background = `radial-gradient(circle at ${glowX}% ${glowY}%, rgb(184, 196, 211), transparent)`;
+};
+
+const handleMouseleave = () => {
+  picture.value.style.transform = `rotateX(0deg) rotateY(0deg) scale(1)`;
+  glow.value.style.transform = `rotateX(0deg) rotateY(0deg) scale(1)`;
+  glow.value.style.background = "";
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  perspective: 1000px;
+  @apply h-80 w-56 flex;
+
+  picture {
+    @apply h-full w-full rounded overflow-hidden duration-200;
+  }
+
+  picture img {
+    @apply h-full w-full object-cover;
+  }
+
+  .glow {
+    background: radial-gradient(
+      circle at 50% 0%,
+      rgb(184, 196, 211),
+      transparent
+    );
+    @apply h-full w-full absolute top-0 left-0 rounded mix-blend-hard-light duration-200;
+  }
+
+  .frame {
+    @apply h-full w-full absolute top-5 left-5 -z-10 rounded border border-teal-200;
+  }
+}
+</style>
