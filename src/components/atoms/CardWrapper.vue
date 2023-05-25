6f67bc5c2fcb0212de333ca077d8a16773a50545
g4rf4z <gregoire.pinte@gmail.com>
2023-05-25 16:24:13 +0200

    Custom cardWrapper

--- src/components/atoms/CardWrapper.vue
+++ src/components/atoms/CardWrapper.vue
@@ -1,13 +1,25 @@
 <template>
-  <article>
+  <article :class="class">
     <slot></slot>
   </article>
 </template>
 
-<script setup></script>
+<script setup>
+const props = defineProps({
+  class: { type: String, default: null },
+});
+</script>
 
 <style lang="scss" scoped>
 article {
-  @apply p-5 rounded bg-slate-800;
+  @apply p-5 rounded duration-300;
+}
+
+article[class='skill-card'] {
+  @apply bg-slate-800/50 shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] drop-shadow-lg;
+}
+
+article[class='experience-card'] {
+  @apply hover:bg-slate-800/50 hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] hover:drop-shadow-lg;
 }
 </style>
--- src/components/organisms/ExperiencesList.vue
+++ src/components/organisms/ExperiencesList.vue
@@ -1,6 +1,13 @@
 <template>
   <ul>
-    <li v-for="(experience, index) in sortedExperiences" :key="index">
+    <li
+      v-for="(experience, index) in sortedExperiences"
+      :key="index"
+      :class="{
+        dimmed:
+          hoveredExperienceId !== null && experience.id !== hoveredExperienceId,
+      }"
+    >
       <experience-card-molecule
         :id="experience.id"
         :position="experience.position"
@@ -10,6 +17,8 @@
         :from="experience.fromToString"
         :to="experience.toToString"
         :tasks="experience.tasks"
+        @mouseover="hoveredCard(experience.id)"
+        @mouseleave="unhovered"
       ></experience-card-molecule>
     </li>
     <icon-link-molecule
@@ -23,12 +32,12 @@
 </template>
 
 <script setup>
-import ExperienceCardMolecule from "#/molecules/ExperienceCard.vue";
-import IconLinkMolecule from "#/molecules/IconLink.vue";
+import ExperienceCardMolecule from '#/molecules/ExperienceCard.vue';
+import IconLinkMolecule from '#/molecules/IconLink.vue';
 
-import { computed } from "vue";
-import { useExperienceStore } from "@/store/experienceStore";
-import { useAuthenticationStore } from "@/store/authenticationStore";
+import { computed, ref } from 'vue';
+import { useExperienceStore } from '@/store/experienceStore';
+import { useAuthenticationStore } from '@/store/authenticationStore';
 
 const experienceStore = useExperienceStore();
 const authenticationStore = useAuthenticationStore();
@@ -49,15 +58,39 @@ const retrieveExperiences = async () => {
   }
 };
 retrieveExperiences();
+
+let hoveredExperienceId = ref(null);
+
+const hoveredCard = (id) => {
+  hoveredExperienceId.value = id;
+};
+
+const unhovered = () => {
+  hoveredExperienceId.value = null;
+};
 </script>
 
 <style lang="scss" scoped>
 ul {
   @apply flex flex-col space-y-5;
 
+  li.dimmed {
+    @apply opacity-25;
+  }
+
   .icon-link {
     @apply p-5 flex items-center justify-center text-indigo-200 bg-slate-800/25;
     @apply hover:text-teal-200 hover:bg-slate-800 hover:duration-300;
   }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0.5;
 }
 </style>
