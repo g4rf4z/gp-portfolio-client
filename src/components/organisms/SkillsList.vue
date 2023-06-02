2c6972671ef4aceb2bb1efb3e58c0e562f04fe58
g4rf4z <gregoire.pinte@gmail.com>
2023-06-02 13:42:46 +0200

    Integrate "usePagination" generic function

--- src/components/organisms/SkillsList.vue
+++ src/components/organisms/SkillsList.vue
@@ -1,6 +1,6 @@
 <template>
   <ul>
-    <li v-for="(skill, index) in skills" :key="index">
+    <li v-for="(skill, index) in paginatedSkills" :key="index">
       <skill-card-molecule
         :id="skill.id"
         :name="skill.name"
@@ -8,23 +8,35 @@
         :progress="skill.progress"
       ></skill-card-molecule>
     </li>
-    <icon-link-molecule
-      v-if="session && admin"
-      to="/create-skill"
-      weight="fas"
-      icon="plus"
-      class="creation-icon"
-    ></icon-link-molecule>
+    <li>
+      <icon-link-molecule
+        v-if="session && admin"
+        to="/create-skill"
+        weight="fas"
+        icon="plus"
+        class="creation-icon"
+      ></icon-link-molecule>
+    </li>
   </ul>
+  <div class="button-wrapper">
+    <button-atom
+      @click="dataLoaded ? displayLessSkills() : displayMoreSkills()"
+    >
+      {{ dataLoaded ? 'Voir moins' : 'Voir plus' }}
+    </button-atom>
+  </div>
 </template>
 
 <script setup>
 import SkillCardMolecule from '#/molecules/SkillCard.vue';
 import IconLinkMolecule from '#/molecules/IconLink.vue';
+import ButtonAtom from '#/atoms/Button.vue';
 
 import { computed } from 'vue';
 import { useSkillStore } from '@/store/skillStore';
 import { useAuthenticationStore } from '@/store/authenticationStore';
+
+import { usePagination } from '@/utils/usePagination';
 
 const skillStore = useSkillStore();
 const authenticationStore = useAuthenticationStore();
@@ -41,16 +53,27 @@ const retrieveSkills = async () => {
   }
 };
 retrieveSkills();
+
+const {
+  paginatedData: paginatedSkills,
+  displayMoreData: displayMoreSkills,
+  displayLessData: displayLessSkills,
+  dataLoaded,
+} = usePagination(skills, 5);
 </script>
 
 <style lang="scss" scoped>
 ul {
   @apply flex flex-col space-y-5;
   @apply md:grid md:grid-cols-2 md:gap-5 md:space-y-0 2xl:grid 2xl:grid-cols-3 2xl:gap-5 2xl:space-y-0;
+
+  .creation-icon {
+    @apply h-[152px] flex items-center justify-center rounded text-indigo-200 bg-slate-800/25 duration-300;
+    @apply hover:text-teal-200 hover:bg-slate-800/50;
+  }
 }
 
-.creation-icon {
-  @apply p-5 flex items-center justify-center duration-300 text-indigo-200 bg-slate-800/25;
-  @apply hover:text-teal-200 hover:bg-slate-800;
+.button-wrapper {
+  @apply mt-5 flex items-center justify-center;
 }
 </style>
