28160c394052d7da92813debea51cee3a46817bc
Grégoire Pinte <94374117+g4rf4z@users.noreply.github.com>
2023-06-02 15:01:02 +0200

    [2306][GP] - Development of new features (#4)
    * Improve "useToggleAnimation" function
    
    * Create "usePagination" generic function
    
    * Integrate "usePagination" generic function
    
    * Improve "usePagination" generic function
    
    * Update "ExperienceList"

--- src/components/organisms/SkillsList.vue
+++ src/components/organisms/SkillsList.vue
@@ -1,30 +1,43 @@
 <template>
   <ul>
-    <li v-for="(skill, index) in skills" :key="index">
+    <li v-for="(skill, index) in paginatedSkills" :key="index">
       <skill-card-molecule
         :id="skill.id"
         :name="skill.name"
         :image="skill.image"
         :progress="skill.progress"
-      />
+      ></skill-card-molecule>
     </li>
-    <icon-link-molecule
-      v-if="session && admin"
-      to="/create-skill"
-      weight="fas"
-      icon="plus"
-      class="creation-icon"
-    ></icon-link-molecule>
+    <li>
+      <icon-link-molecule
+        v-if="session && admin"
+        to="/create-skill"
+        weight="fas"
+        icon="plus"
+        class="creation-icon"
+      ></icon-link-molecule>
+    </li>
   </ul>
+  <div class="button-wrapper">
+    <button-atom
+      v-if="pageSize >= 5"
+      @click="dataLoaded ? displayLessSkills() : displayMoreSkills()"
+    >
+      {{ dataLoaded ? 'Voir moins' : 'Voir plus' }}
+    </button-atom>
+  </div>
 </template>
 
 <script setup>
-import SkillCardMolecule from "#/molecules/SkillCard.vue";
-import IconLinkMolecule from "#/molecules/IconLink.vue";
+import SkillCardMolecule from '#/molecules/SkillCard.vue';
+import IconLinkMolecule from '#/molecules/IconLink.vue';
+import ButtonAtom from '#/atoms/Button.vue';
 
-import { computed } from "vue";
-import { useSkillStore } from "@/store/skillStore";
-import { useAuthenticationStore } from "@/store/authenticationStore";
+import { computed } from 'vue';
+import { useSkillStore } from '@/store/skillStore';
+import { useAuthenticationStore } from '@/store/authenticationStore';
+
+import { usePagination } from '@/utils/usePagination';
 
 const skillStore = useSkillStore();
 const authenticationStore = useAuthenticationStore();
@@ -41,16 +54,28 @@ const retrieveSkills = async () => {
   }
 };
 retrieveSkills();
+
+const {
+  pageSize,
+  paginatedData: paginatedSkills,
+  displayMoreData: displayMoreSkills,
+  displayLessData: displayLessSkills,
+  dataLoaded,
+} = usePagination(skills, 5);
 </script>
 
 <style lang="scss" scoped>
 ul {
   @apply flex flex-col space-y-5;
   @apply md:grid md:grid-cols-2 md:gap-5 md:space-y-0 2xl:grid 2xl:grid-cols-3 2xl:gap-5 2xl:space-y-0;
+
+  .creation-icon {
+    @apply h-[152px] flex items-center justify-center rounded text-indigo-200 bg-slate-800/25 duration-300;
+    @apply hover:text-teal-200 hover:bg-slate-800/50;
+  }
 }
 
-.creation-icon {
-  @apply p-5 flex items-center justify-center duration-300 text-indigo-200 bg-slate-800/25;
-  @apply hover:text-teal-200 hover:bg-slate-800;
+.button-wrapper {
+  @apply mt-5 flex items-center justify-center;
 }
 </style>
---
+++ src/utils/usePagination.js
@@ -0,0 +1,36 @@
+import { ref, computed } from 'vue';
+
+export const usePagination = (data, pageSize) => {
+  const page = ref(1);
+  const dataLoaded = ref(false);
+
+  const paginatedData = computed(() => {
+    return data.value.slice(0, page.value * pageSize);
+  });
+
+  const displayMoreData = () => {
+    if (page.value * pageSize < data.value.length) {
+      page.value++;
+      if (page.value * pageSize >= data.value.length) {
+        dataLoaded.value = true;
+      }
+    }
+  };
+
+  const displayLessData = () => {
+    if (page.value > 1) {
+      page.value--;
+    }
+    if (page.value <= 1) {
+      dataLoaded.value = false;
+    }
+  };
+
+  return {
+    pageSize,
+    paginatedData,
+    displayMoreData,
+    displayLessData,
+    dataLoaded,
+  };
+};
