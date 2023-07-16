13e1ddaab3407758bdd4db830c430ff40533e0b7
Grégoire Pinte <94374117+g4rf4z@users.noreply.github.com>
2023-07-16 13:41:25 +0200

    [2307][GP] - Updating packages (#5)

--- src/store/skillStore.js
+++ src/store/skillStore.js
@@ -1,9 +1,9 @@
-import { defineStore } from "pinia";
-import { Skill } from "@/models/Skill.model";
-import { apiWrapper } from "@/services/api";
+import { defineStore } from 'pinia';
+import { Skill } from '@/models/Skill.model';
+import { apiWrapper } from '@/services/api';
 
 export const useSkillStore = defineStore({
-  id: "skillStore",
+  id: 'skillStore',
   state: () => ({
     skill: {},
     skills: [],
@@ -22,29 +22,29 @@ export const useSkillStore = defineStore({
   actions: {
     async createSkill({ name, image, progress }) {
       try {
-        this.loaders["createSkill"] = true;
+        this.loaders['createSkill'] = true;
         const createdSkill = await apiWrapper.post(`/skills`, {
           data: { name, image, progress },
         });
         return (this.skill = new Skill(createdSkill));
       } finally {
-        this.loaders["createSkill"] = false;
+        this.loaders['createSkill'] = false;
       }
     },
 
     async retrieveSkill(id) {
       try {
-        this.loaders["retrieveSkill"] = true;
+        this.loaders['retrieveSkill'] = true;
         const retrievedSkill = await apiWrapper.get(`/skills/${id}`);
         return (this.skill = new Skill(retrievedSkill));
       } finally {
-        this.loaders["retrieveSkill"] = false;
+        this.loaders['retrieveSkill'] = false;
       }
     },
 
     async retrieveSkills() {
       try {
-        this.loaders["retrieveSkills"] = true;
+        this.loaders['retrieveSkills'] = true;
         const retrievedSkills = await apiWrapper.get(`/skills`);
         const skillsList = [];
         retrievedSkills.forEach((skill) => {
@@ -52,25 +52,25 @@ export const useSkillStore = defineStore({
         });
         return (this.skills = skillsList);
       } finally {
-        this.loaders["retrieveSkills"] = false;
+        this.loaders['retrieveSkills'] = false;
       }
     },
 
     async updateSkill(id, { name, image, progress }) {
       try {
-        this.loaders["updateSkill"] = true;
+        this.loaders['updateSkill'] = true;
         const updatedSkill = await apiWrapper.patch(`/skills/${id}`, {
           data: { name, image, progress },
         });
         return (this.skill = new Skill(updatedSkill));
       } finally {
-        this.loaders["updateSkill"] = false;
+        this.loaders['updateSkill'] = false;
       }
     },
 
     async deleteSkill(id) {
       try {
-        this.loaders["deleteSkill"] = true;
+        this.loaders['deleteSkill'] = true;
         const deletedSkill = await apiWrapper.delete(`/skills/${id}`);
         const deletedSkillIndex = this.skills.findIndex(
           (skill) => skill.id === deletedSkill.id
@@ -78,7 +78,7 @@ export const useSkillStore = defineStore({
         this.skills.splice(deletedSkillIndex, 1);
         return new Skill(deletedSkill);
       } finally {
-        this.loaders["deleteSkill"] = false;
+        this.loaders['deleteSkill'] = false;
       }
     },
   },
